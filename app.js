9de13752540d96740c6575d6d7029c4fed2010a2
Kevin <k.lassauniere@live.fr>
2022-10-10 16:53:39 +0200

    Set up heroku local

--- app.js
+++ app.js
@@ -30,3 +30,5 @@ const usersRouter = require("./routes/users");
 app.use("/", indexRouter); // Redirect to '/posts', necessary to avoid CastError
 app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
+
+module.exports = app;
