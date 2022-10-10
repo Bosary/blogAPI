9b8fd83c32c823cfcd970bc7d436e46efefbe57f
Kevin <k.lassauniere@live.fr>
2022-10-10 17:23:31 +0200

    Heroku fix

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const indexRouter = require("./routes/index");
 const postsRouter = require("./routes/posts");
 const usersRouter = require("./routes/users");
 
-app.use("/api/", indexRouter); // Redirect to '/posts', necessary to avoid CastError
+app.use("/", indexRouter); // Redirect to '/posts', necessary to avoid CastError
 app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
 
