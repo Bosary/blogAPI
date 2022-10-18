989fe271c0cd94b31ad677c600e19073e88104b7
Kevin <k.lassauniere@live.fr>
2022-10-18 15:07:39 +0200

    cors fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ require("./config/database");
  */
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
